d652b98764a17d41b16a68129308b5baefdf1802
Saka Azeez Olamilekan <89335912+sao-concept@users.noreply.github.com>
2024-01-04 16:19:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    this.name = "skillsMember";
+    this.html = '<div class="skillsMember"></div>';
+    this.css = '.skillsMember {background-color: #f1f1f1; width: 100%; height: 100%;}';
+    this.script = '';
+}
